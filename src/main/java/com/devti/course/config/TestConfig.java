e2022807085f0db608341cac7af137e81f7d02fc
Victor Oliveira <victor.souza.41@hotmail.com>
2020-07-10 21:42:45 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/devti/course/config/TestConfig.java
+++ src/main/java/com/devti/course/config/TestConfig.java
@@ -22,44 +22,58 @@ import com.devti.course.repositories.UserRepository;
 @Profile("test")
 public class TestConfig implements CommandLineRunner {
 
-	//Com essa anotação o próprio Spring resolve a dependencia e associar uma instância
-	@Autowired 
+	// Com essa anotação o próprio Spring resolve a dependencia e associar uma
+	// instância
+	@Autowired
 	private UserRepository userRepository;
-	
+
 	@Autowired
 	private OrderRepository orderRepository;
-	
+
 	@Autowired
 	private CategoryRepository categoryRepositoy;
-	
+
 	@Autowired
 	private ProductRepository productRepository;
 
-	//Povoando banco
+	// Povoando banco
 	@Override
 	public void run(String... args) throws Exception {
-		
+
 		Category cat1 = new Category(null, "Electronics");
 		Category cat2 = new Category(null, "Books");
-		Category cat3 = new Category(null, "Computers"); 
-		
+		Category cat3 = new Category(null, "Computers");
+
+		categoryRepositoy.saveAll(Arrays.asList(cat1, cat2, cat3));
+
 		Products p1 = new Products(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
 		Products p2 = new Products(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
 		Products p3 = new Products(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
 		Products p4 = new Products(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
-		Products p5 = new Products(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, ""); 
-		
+		Products p5 = new Products(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
+
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+
+		p1.getCategories().add(cat2); // O produto p1 está associado ao objeto cat2
+		p2.getCategories().add(cat1);
+		p2.getCategories().add(cat3);
+		p3.getCategories().add(cat3);
+		p4.getCategories().add(cat3);
+		p5.getCategories().add(cat2);
+
+		// Salvando as associações acima
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
-		
+		// Salvando usuário no banco
+		userRepository.saveAll(Arrays.asList(u1, u2));
+
 		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.Aguardando_Pagamento, u1);
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.Pago, u2);
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.Cancelado, u1); 
-		
-		//Salvando usuário no banco
-		userRepository.saveAll(Arrays.asList(u1, u2));
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.Cancelado, u1);
+
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
-		categoryRepositoy.saveAll(Arrays.asList(cat1, cat2, cat3));
-		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4));
+
 	}
 }
--- src/main/java/com/devti/course/entities/Products.java
+++ src/main/java/com/devti/course/entities/Products.java
@@ -8,8 +8,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
-import javax.persistence.Transient;
 
 @Entity
 @Table(name = "tb_product")
@@ -24,7 +26,11 @@ public class Products implements Serializable {
 	private Double price;
 	private String imgUrl;
 
-	@Transient
+	// Mapeamento para transformar a coleção na tabela de associação do modelo
+	// relacional
+	// inverse = Define qual a chave estrangeira da outra entidade
+	@ManyToMany
+	@JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();
 
 	public Products() {
