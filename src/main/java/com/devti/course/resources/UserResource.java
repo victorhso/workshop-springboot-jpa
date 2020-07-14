db848090b35524f32f656b3eef4c5450ab605897
Victor Oliveira <victor.souza.41@hotmail.com>
2020-07-14 19:33:01 -0300

    User insert

--- src/main/java/com/devti/course/resources/UserResource.java
+++ src/main/java/com/devti/course/resources/UserResource.java
@@ -1,13 +1,17 @@
 package com.devti.course.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.devti.course.entities.User;
 import com.devti.course.services.UserService;
@@ -15,20 +19,27 @@ import com.devti.course.services.UserService;
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
-	
+
 	@Autowired
 	private UserService service;
-	
-	//Retorna respostas de requisições web
-	@GetMapping //Indica que esse método responde a requisição do tipo Get do HTTP
-	public ResponseEntity<List<User>> findAll(){
-		List <User> list = service.findAll();
-		return ResponseEntity.ok().body(list); //Retorna resposta com sucesso / retornar corpo da resposta
+
+	// Retorna respostas de requisições web
+	@GetMapping // Indica que esse método responde a requisição do tipo Get do HTTP
+	public ResponseEntity<List<User>> findAll() {
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list); // Retorna resposta com sucesso / retornar corpo da resposta
 	}
-	
-	@GetMapping(value = "/{id}") //Diz que a URL terá um parâmetro
+
+	@GetMapping(value = "/{id}") // Diz que a URL terá um parâmetro
 	public ResponseEntity<User> findById(@PathVariable Long id) {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
 	}
 }
--- src/main/java/com/devti/course/services/UserService.java
+++ src/main/java/com/devti/course/services/UserService.java
@@ -9,19 +9,23 @@ import org.springframework.stereotype.Service;
 import com.devti.course.entities.User;
 import com.devti.course.repositories.UserRepository;
 
-@Service //Registra a classe como componente do Spring
+@Service // Registra a classe como componente do Spring
 public class UserService {
 
-	@Autowired //Para que o spring faça a injenção de dependência
+	@Autowired // Para que o spring faça a injenção de dependência
 	private UserRepository repository;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.get();
 	}
-}
 
+	// Inserindo no BD um novo objeto user
+	public User insert(User obj) {
+		return repository.save(obj);
+	}
+}
